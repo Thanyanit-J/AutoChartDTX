db45ce2be30ac0c41a1b0a11634fe08d0f8f4019
Thanyanit Jongjitragan <46578644+Thanyanit-J@users.noreply.github.com>
2021-11-15 17:17:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,21 +15,22 @@ Convert a song to Simfile.
 # st.file_uploader get input in Bytes, so we need to read() it before progressing.
 inputData = st.file_uploader('Upload a song', type=['wav', 'mp3']) 
 
-simfile = open('Letters Goodbye.zip', 'r')
-def get_table_download_link():
-    href = f'<a href="https://drive.google.com/u/0/uc?id=17Ahl-368opQc8Xmi1nIiN4S8H0b5S0BP&export=download" download>Download Simfile</a>'
-    return href
+# Obsolete workaround to download stuf in streamlit older version
+# simfile = open('Letters Goodbye.zip', 'r')
+# def get_table_download_link():
+#     href = f'<a href="https://drive.google.com/u/0/uc?id=17Ahl-368opQc8Xmi1nIiN4S8H0b5S0BP&export=download" download>Download Simfile</a>'
+#     return href
     
 if inputData != None:
 #     song = inputData.read()
     
     
-#     st.download_button(
-#          label="Download Simfile",
-#          data=Simfile,
-#          file_name="Letters Goodbye.zip",
-#        )
-    st.markdown(get_table_download_link(), unsafe_allow_html=True)
+    st.download_button(
+         label="Download Simfile",
+         data=Simfile,
+         file_name="Letters Goodbye.zip",
+       )
+#     st.markdown(get_table_download_link(), unsafe_allow_html=True)
 
 #     st.image(image)
     # loaded_model = tf.keras.models.load_model('/model/acc0.8685')

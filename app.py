39b893be7ef2cc4beb54ed9357d11a6183d64588
Thanyanit Jongjitragan <tun@TunMacbook.local>
2022-03-22 22:55:55 +0700

    test streamlit with librosa

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
+import librosa
+import librosa.display
 
 st.write("""
 # Demo of our project!
@@ -13,26 +14,23 @@ Convert a song to Simfile.
 """)
 
 # st.file_uploader get input in Bytes, so we need to read() it before progressing.
-inputData = st.file_uploader('Upload a song', type=['wav', 'mp3']) 
+uploadedSong = st.file_uploader('Upload a song', type=['wav', 'mp3']) 
 
-simfile = open('Letters Goodbye.zip', 'rb')
+# simfile = open('Letters Goodbye.zip', 'rb')
 
 # Obsolete workaround to download stuf in streamlit older version
 # def get_table_download_link():
 #     href = f'<a href="https://drive.google.com/u/0/uc?id=17Ahl-368opQc8Xmi1nIiN4S8H0b5S0BP&export=download" download>Download Simfile</a>'
 #     return href
     
-if inputData != None:
+if uploadedSong != None:
 #     song = inputData.read()
+    song, _ = librosa.load(uploadedSong, sr=44100)
     
-    
-    st.download_button(
-         label="Download Simfile",
-         data=simfile,
-         file_name="Letters Goodbye.zip",
-       )
-#     st.markdown(get_table_download_link(), unsafe_allow_html=True)
-
-#     st.image(image)
-    # loaded_model = tf.keras.models.load_model('/model/acc0.8685')
+    st.write(len(song))
+    # st.download_button(
+    #      label="Download Simfile",
+    #      data=simfile,
+    #      file_name="Letters Goodbye.zip",
+    #    )
 
